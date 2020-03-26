c1fa2fdaba2a12fc9b9ba77e67d12c29c9f99e88
Kevin Arutyunyan <d3d9@riseup.net>
2020-03-26 20:18:56 +0100

    fehlende Dateien..

---
+++ js/leaflet-messagebox.js
@@ -0,0 +1,62 @@
+/* src: https://github.com/tinuzz/leaflet-messagebox
+
+Copyright (c) 2015, Martijn Grendelman
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of leaflet-messagebox nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+L.Control.Messagebox = L.Control.extend({
+    options: {
+        position: 'topright',
+        timeout: 3000
+    },
+
+    onAdd: function (map) {
+        this._container = L.DomUtil.create('div', 'leaflet-control-messagebox');
+        L.DomEvent.disableClickPropagation(this._container);
+        return this._container;
+    },
+
+    show: function (message, timeout) {
+        var elem = this._container;
+        elem.innerHTML = message;
+        elem.style.display = 'block';
+
+        timeout = timeout || this.options.timeout;
+
+        if (typeof this.timeoutID == 'number') {
+            clearTimeout(this.timeoutID);
+        }
+        this.timeoutID = setTimeout(function () {
+            elem.style.display = 'none';
+        }, timeout);
+    }
+});
+
+L.control.messagebox = function (options) {
+    return new L.Control.Messagebox(options);
+};
